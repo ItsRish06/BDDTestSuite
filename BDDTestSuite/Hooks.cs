85e660c1e019dbe1939011c2ab8e004d6c24adc7
Rishab Shetty <rishab.shetty06@gmail.com>
2025-05-18 22:10:59 +0530

    add config file and initialize config in hooks

---
+++ BDDTestSuite/Hooks.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Reqnroll.BoDi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDTestSuite
+{
+    [Binding]
+    public class Hooks
+    {
+        private readonly IObjectContainer _objectContainer;
+        private static IConfigurationRoot? _configuration;
+        public Hooks(IObjectContainer objectContainer) 
+        {
+            _objectContainer = objectContainer;
+        }
+
+        [BeforeTestRun]
+        public static void TestRunSetUp()
+        {
+            _configuration = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario()
+        {
+            _objectContainer.RegisterInstanceAs<IConfigurationRoot?>(_configuration);
+        }
+    }
+}
