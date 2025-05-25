2bd3f5dfc3fe8cd811a084e2ba17dc588b01fda0
Rishab Shetty <rishab.shetty06@gmail.com>
2025-05-26 02:22:00 +0530

    add browser and wait utils

--- BDDTestSuite/Utils/BrowserUtil.cs
+++ BDDTestSuite/Utils/BrowserUtil.cs
@@ -4,6 +4,7 @@ using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,41 +13,65 @@ namespace BDDTestSuite.Utils
 {
     public class BrowserUtil
     {
-        public static IWebDriver InitializeBrowser(string browserName, bool headless)
+        public static IWebDriver InitializeBrowser(string browserName, bool headless = false)
         {
-            switch (browserName.ToLower()) {
+            ArgumentNullException.ThrowIfNull(browserName, nameof(browserName));
+
+            IWebDriver driver;
+
+            switch (browserName.ToLower())
+            {
                 case "chrome":
                     var chromeOptions = new ChromeOptions();
                     chromeOptions.AddArgument("--no-sandbox");
                     chromeOptions.AddArgument("--disable-dev-shm-usage");
                     chromeOptions.AddArgument("--incognito");
+                    chromeOptions.AddArgument("--maximize-window");
                     if (headless)
                         chromeOptions.AddArgument("--headless");
-                    return new ChromeDriver(chromeOptions);
+
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
 
                 case "firefox":
                     var firefoxOptions = new FirefoxOptions();
                     firefoxOptions.AddArgument("--no-sandbox");
                     firefoxOptions.AddArgument("--disable-dev-shm-usage");
                     firefoxOptions.AddArgument("--incognito");
+                    firefoxOptions.AddArgument("--maximize-window");
                     if (headless)
                         firefoxOptions.AddArgument("--headless");
-                    return new FirefoxDriver(firefoxOptions);
+
+                    driver = new FirefoxDriver(firefoxOptions);
+                    break;
 
                 case "edge":
                     var edgeOptions = new EdgeOptions();
                     edgeOptions.AddArgument("--no-sandbox");
                     edgeOptions.AddArgument("--disable-dev-shm-usage");
                     edgeOptions.AddArgument("--incognito");
+                    edgeOptions.AddArgument("--maximize-window");
                     if (headless)
                         edgeOptions.AddArgument("--headless");
-                    return new EdgeDriver(edgeOptions);
+
+                    driver = new EdgeDriver(edgeOptions);
+                    break;
 
                 default:
                     throw new ArgumentException($"Browser '{browserName}' is not supported.");
-
             }
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+            return driver;
         }
-        
+
+        public static void NavigateToUrl(IWebDriver driver, string url)
+        {
+            ArgumentNullException.ThrowIfNull(driver, nameof(driver));
+            ArgumentNullException.ThrowIfNull(url, nameof(url));
+
+            driver.Navigate().GoToUrl(url);
+        }
     }
 }
---
+++ BDDTestSuite/Utils/WaitUtils.cs
@@ -0,0 +1,98 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDTestSuite.Utils
+{ 
+    public class WaitUtils
+    {
+        public static IWebElement WaitUntilElementIsVisible(IWebDriver driver, By by, TimeSpan timeout)
+        {
+            ArgumentNullException.ThrowIfNull(driver, nameof(driver));
+            ArgumentNullException.ThrowIfNull(by, nameof(by));
+
+            var wait = new WebDriverWait(driver, timeout);
+
+            wait.IgnoreExceptionTypes(
+                typeof(StaleElementReferenceException),
+                typeof(NoSuchElementException)
+                );
+
+            try
+            {
+                return wait.Until(d =>
+                {
+                    IWebElement element = d.FindElement(by);
+                    if (!element.Displayed)
+                        return null;
+
+                    return element;
+                });
+            }
+            catch (Exception ex) 
+            { 
+                throw new WebDriverTimeoutException($"Element {by} is not visible even after {timeout}",ex);
+            }
+            
+        }
+        public static IWebElement WaitUntilElementIsClickable(IWebDriver driver, By by, TimeSpan timeout)
+        {
+            ArgumentNullException.ThrowIfNull(driver, nameof(driver));
+            ArgumentNullException.ThrowIfNull(by, nameof(by));
+
+            var wait = new WebDriverWait(driver, timeout);
+
+            wait.IgnoreExceptionTypes(
+                typeof(ElementClickInterceptedException),
+                typeof(StaleElementReferenceException), 
+                typeof(NoSuchElementException)
+                );
+
+            try
+            {
+                return wait.Until(d =>
+                {
+                    IWebElement element = d.FindElement(by);
+                    if (!element.Displayed || !element.Enabled)
+                        return null;
+
+                    return element;
+                });
+            }
+            catch (Exception ex) 
+            {
+                throw new WebDriverTimeoutException($"Element {by} still not clickable after {timeout}", ex);
+            }
+            
+        }
+
+        public static bool WaitUntilUrlChangesTo(IWebDriver driver, string url, TimeSpan timeout)
+        {
+            ArgumentNullException.ThrowIfNullOrEmpty(url, nameof(url));
+            ArgumentNullException.ThrowIfNull(timeout, nameof(timeout));
+
+            var wait = new WebDriverWait(driver, timeout);
+            try
+            {
+                return wait.Until(d =>
+                {
+                    string currentUrl = d.Url;
+                    if (currentUrl.Equals(url))
+                        return true;
+
+                    return false;
+                });
+            }
+            catch (Exception ex) 
+            {
+                throw new WebDriverTimeoutException($"URL did not change to {url} after {timeout}. Current URL: {driver.Url}", ex);
+            }
+            
+
+        }
+    }
+}
