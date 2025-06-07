2408ab2d30f70a826b37485b060008b50a722b68
Rishab Shetty <rishab.shetty06@gmail.com>
2025-06-08 01:27:53 +0530

    add ElementUtils and more type of waits in WaitUtils

---
+++ BDDTestSuite/Utils/ElementUtils.cs
@@ -0,0 +1,27 @@
+﻿using AngleSharp.Dom;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDTestSuite.Utils
+{
+    public static class ElementUtils
+    {
+        public static void JSClick(IWebDriver driver, IWebElement element)
+        {
+            try
+            {
+                var js = (IJavaScriptExecutor)driver;
+                js.ExecuteScript("arguments[0].click();", element);
+
+            }catch (Exception ex)
+            {
+                throw new Exception($"Unexpected error during JS click: {ex.Message}");
+            }
+            
+        }
+    }
+}
--- BDDTestSuite/Utils/WaitUtils.cs
+++ BDDTestSuite/Utils/WaitUtils.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -33,7 +34,7 @@ namespace BDDTestSuite.Utils
                     return element;
                 });
             }
-            catch (Exception ex) 
+            catch (WebDriverTimeoutException ex) 
             { 
                 throw new WebDriverTimeoutException($"Element {by} is not visible even after {timeout}",ex);
             }
@@ -63,17 +64,17 @@ namespace BDDTestSuite.Utils
                     return element;
                 });
             }
-            catch (Exception ex) 
+            catch (WebDriverTimeoutException ex) 
             {
-                throw new WebDriverTimeoutException($"Element {by} still not clickable after {timeout}", ex);
+                throw new WebDriverTimeoutException($"Timed out after {timeout} waiting for element located by {by} to be clickable", ex);
             }
             
         }
 
         public static bool WaitUntilUrlChangesTo(IWebDriver driver, string url, TimeSpan timeout)
         {
+            ArgumentNullException.ThrowIfNull(driver, nameof(driver));
             ArgumentNullException.ThrowIfNullOrEmpty(url, nameof(url));
-            ArgumentNullException.ThrowIfNull(timeout, nameof(timeout));
 
             var wait = new WebDriverWait(driver, timeout);
             try
@@ -92,6 +93,92 @@ namespace BDDTestSuite.Utils
                 throw new WebDriverTimeoutException($"URL did not change to {url} after {timeout}. Current URL: {driver.Url}", ex);
             }
             
+        }
+
+        public static bool WaitUntilUrlContains(IWebDriver driver, string urlString, TimeSpan timeout)
+        {
+            ArgumentNullException.ThrowIfNull(driver, nameof(driver));
+            ArgumentNullException.ThrowIfNullOrEmpty(urlString, nameof(urlString));
+
+            var wait = new WebDriverWait(driver, timeout);
+            try
+            {
+                return wait.Until(d =>
+                {
+                    string currentUrl = d.Url;
+                    if (currentUrl.Contains(urlString))
+                        return true;
+
+                    return false;
+                });
+            }
+            catch (Exception ex)
+            {
+                throw new WebDriverTimeoutException($"The current Url did not contain the string - {urlString}. Current URL: {driver.Url}", ex);
+            }
+
+        }
+
+        public static IList<IWebElement> WaitUntilAllElementsAreClickable(IWebDriver driver, By by, TimeSpan timeout)
+        {
+            ArgumentNullException.ThrowIfNull(driver, nameof(driver));
+            ArgumentNullException.ThrowIfNull(by, nameof(by));
+
+            var wait = new WebDriverWait(driver, timeout);
+
+            wait.IgnoreExceptionTypes(
+                typeof(ElementClickInterceptedException),
+                typeof(StaleElementReferenceException),
+                typeof(NoSuchElementException)
+                );
+
+            try
+            {
+                return wait.Until(d =>
+                {
+                    var elements = d.FindElements(by);
+                    if (elements.All(element => element.Displayed && element.Enabled))
+                        return elements;
+
+                    return null;
+
+                });
+            }
+            catch (WebDriverTimeoutException ex)
+            {
+                throw new WebDriverTimeoutException($"Timed out after {timeout} waiting for all elements located by {by} to be clickable", ex);
+            }
+
+        }
+
+        public static IList<IWebElement> WaitUntilAllElementsAreVisible(IWebDriver driver, By by, TimeSpan timeout)
+        {
+            ArgumentNullException.ThrowIfNull(driver, nameof(driver));
+            ArgumentNullException.ThrowIfNull(by, nameof(by));
+
+            var wait = new WebDriverWait(driver, timeout);
+
+            wait.IgnoreExceptionTypes(
+                typeof(StaleElementReferenceException),
+                typeof(NoSuchElementException)
+                );
+
+            try
+            {
+                return wait.Until(d =>
+                {
+                    var elements = d.FindElements(by);
+                    if (elements.All(element => element.Displayed))
+                        return elements;
+
+                    return null;
+
+                });
+            }
+            catch (WebDriverTimeoutException ex)
+            {
+                throw new WebDriverTimeoutException($"Timed out after {timeout} waiting for all elements located by {by} to be clickable", ex);
+            }
 
         }
     }
