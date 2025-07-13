91f73c154a9ce755b3da982a55100eeb3d7c5bfc
Rishab Shetty <rishab.shetty06@gmail.com>
2025-07-13 17:55:15 +0530

    add report and screenshot utils

---
+++ BDDTestSuite/Utils/ReportUtils.cs
@@ -0,0 +1,49 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.MarkupUtils;
+using Reqnroll;
+using Serilog.Core;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDTestSuite.Utils
+{
+    public class ReportUtils
+    {
+        public static ExtentTest CreateStepNode(ExtentTest scenario, ScenarioBlock scenarioBlock, string stepInfo, string stepDesc = "" )
+        {
+            ExtentTest node;
+
+            switch (scenarioBlock)
+            {
+                case ScenarioBlock.Given:
+                    node = scenario.CreateNode<Given>(stepInfo, stepDesc);
+                    break;
+                case ScenarioBlock.When:
+                    node = scenario.CreateNode<When>(stepInfo, stepDesc);
+                    break;
+                case ScenarioBlock.Then:
+                    node = scenario.CreateNode<Then>(stepInfo, stepDesc);
+                    break;
+                default:
+                    node = scenario.CreateNode<And>(stepInfo, stepDesc);
+                    break;
+            }
+
+            return node;
+        }
+
+        public static void LogFailure(ExtentTest node, Exception exception, string base64Img)
+        {
+            node.Log(Status.Fail, MarkupHelper.CreateCodeBlock(exception.Message));
+            node.Log(Status.Fail, exception);
+            node.Log(Status.Fail, exception.InnerException);
+            node.Fail(MediaEntityBuilder.CreateScreenCaptureFromBase64String(base64Img).Build());
+        }
+    }
+}
---
+++ BDDTestSuite/Utils/ScreenshotUtils.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BDDTestSuite.Utils
+{
+    public class ScreenshotUtils
+    {
+        public static string ScreenCaptureBase64(IWebDriver driver)
+        {
+            string base64 = "";
+
+            if (driver is ChromeDriver chromeDriver)
+            {
+                var metrics = chromeDriver.ExecuteCdpCommand("Page.getLayoutMetrics", new Dictionary<string, object>());
+                var metricsDict = metrics as IDictionary<string, object>;
+                var contentSize = metricsDict?["contentSize"] as IDictionary<string, object>;
+                int width = Convert.ToInt32(contentSize?["width"]);
+                int height = Convert.ToInt32(contentSize?["height"]);
+
+
+                var clip = new Dictionary<string, object>
+                    {
+                        { "x", 0 },
+                        { "y", 0 },
+                        { "width", width },
+                        { "height", height },
+                        { "scale", 1 }
+                    };
+                var screenshotParams = new Dictionary<string, object>
+                    {
+                        { "format", "png" },
+                        { "fromSurface", true },
+                        { "clip", clip }
+                    };
+
+                var result = chromeDriver.ExecuteCdpCommand("Page.captureScreenshot", screenshotParams);
+                var resultDict = result as IDictionary<string, object>;
+
+                base64 = resultDict?["data"] as string;
+            }
+            else if (driver is FirefoxDriver firefoxDriver)
+            {
+                base64 = firefoxDriver.GetFullPageScreenshot().AsBase64EncodedString;
+            }
+
+            return base64;
+        }
+    }
+}
