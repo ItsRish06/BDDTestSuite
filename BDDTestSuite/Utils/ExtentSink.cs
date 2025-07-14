980562b10c430b388bace2275002266e7eb68dd2
Rishab Shetty <rishab.shetty06@gmail.com>
2025-07-14 21:44:57 +0530

    add sink for to collect logs for step nodes

---
+++ BDDTestSuite/Utils/ExtentSink.cs
@@ -0,0 +1,24 @@
+﻿using AventStack.ExtentReports;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDDTestSuite.Utils
+{
+    public class ExtentSink : ILogEventSink
+    {
+        private Queue<string> _logCollection;
+        public ExtentSink(Queue<string> logCollection) 
+        {
+            _logCollection = logCollection;
+        }
+        public void Emit(LogEvent logEvent)
+        {
+            _logCollection.Enqueue(logEvent.RenderMessage());
+        }
+    }
+}
